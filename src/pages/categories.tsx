71c1efcb9a963c3bd79786af89bb6ef73271f7b7
Gilmar Oliveira <giilmaroliveira@outlook.com>
2021-10-25 22:19:12 -0300

    Alterando descrição categoria

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/template/Layout';
 export default function Categories() {
   return (
     <Layout title={'Categorias'} subtitle={'Gestão de categorias'}>
-      Tela de categorias
+      Exibição de categorias
     </Layout>
   );
 }
